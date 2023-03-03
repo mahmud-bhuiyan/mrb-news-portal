f0f313819067a7bb094245e6dcd0e3a76b36931c
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-03-03 21:05:33 +0600

    news section updated

--- scripts/news.js
+++ scripts/news.js
@@ -10,7 +10,6 @@ const showCategories = (data) => {
     const linkContainer = document.createElement("p");
     linkContainer.innerHTML = `
     <a class="nav-link" href="#" onclick="fetchCategoryNews('${singleCategory.category_id}', '${singleCategory?.category_name}')">${singleCategory?.category_name}</a>
-    
     `;
     categoriesContainer.appendChild(linkContainer);
   });
@@ -25,7 +24,63 @@ const fetchCategoryNews = (category_id, category_name) => {
 };
 
 const showAllNews = (data, category_name) => {
-  console.log(data, category_name);
+  //   console.log(data, category_name);
   document.getElementById("news-count").innerText = data.length;
   document.getElementById("category-name").innerText = category_name;
+
+  const newsContainer = document.getElementById("all-news");
+  newsContainer.innerHTML = "";
+  data.forEach((singleNews) => {
+    const { image_url, title, details, author, total_view, rating } =
+      singleNews;
+    // console.log(singleNews);
+    const card = document.createElement("div");
+    card.innerHTML = `
+        <div class="card mb-3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${image_url}" class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-8 d-flex flex-column">
+              <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">
+                ${details.slice(0, 200)}
+                </p>
+              </div>
+              <div class="card-footer border-0 bg-body d-flex justify-content-between">
+                <div>
+                    <img src="${
+                      author.img
+                    }" class="img-fluid rounded-circle" height="40" width="40">
+                    <p class="m-0 p-0">${
+                      author.name ? author.name : "Not available"
+                    }</p>
+                    <p class="m-0 p-0">$author.published_date}</p>
+                </div>
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-eye"></i>
+                    <p class="m-0 p-0">${
+                      total_view ? total_view : "Not available"
+                    }</p>
+                </div>
+                <div class="d-flex gap-2">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half"></i>
+        
+                    <p>${rating.number}</p>
+                </div>
+                <div>
+                    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+    newsContainer.appendChild(card);
+  });
 };
